5e2e1d0b1d74ce74340defb62d3a05e7c186e111
Krishna Sinha <sinhakishu0425@gmail.com>
2026-08-24 23:13:49 +0530

    Docker

---
+++ docker/phase2/Backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port = process.env.PORT;
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("hello from docker phase 2");
+})
+
+app.listen(port, () => {
+    console.log(`server is running at port: ${port}`);
+})
